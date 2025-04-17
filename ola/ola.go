465574535582b93d16538320fad0a5e2efca300d
Gabriel Pedro Paiao <gpaiao@cpqd.com.br>
2025-04-17 11:12:38 -0300

    Finalização da aula 'Olá, mundo'

--- ola/ola.go
+++ ola/ola.go
@@ -2,15 +2,38 @@ package main
 
 import "fmt"
 
-const prefixoOlaPortugues = "Olá, "
+const espanhol = "espanhol"
+const frances = "francês"
+const alemao = "alemão"
 
-func Ola(nome string) string {
+const prefixoOlaPortugues = "Olá, "
+const prefixoOlaEspanhol = "Hola, "
+const prefixoOlaFrances = "Bounjour, "
+const prefixoOlaAlemao = "Hallo, "
+
+func Ola(nome, idioma string) string {
 	if nome == "" {
 		nome = "Mundo"
 	}
-	return prefixoOlaPortugues + nome
+
+	return prefixoDeSaudacao(idioma) + nome
+}
+
+func prefixoDeSaudacao(idioma string) (prefixo string) {
+	switch idioma {
+	case espanhol:
+		prefixo = prefixoOlaEspanhol
+	case frances:
+		prefixo = prefixoOlaFrances
+	case alemao:
+		prefixo = prefixoOlaAlemao
+	default:
+		prefixo = prefixoOlaPortugues
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Ola("mundo"))
+	fmt.Println(Ola("mundo", ""))
 }
